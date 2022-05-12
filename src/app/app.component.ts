d0131e2741b3a618bf33f528f0dbbf321359bd5c
Ross carmack <rosscarmack@Rosss-MBP.lan>
2022-05-12 17:15:15 -0500

    added pdf viewer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import WebViewer from '@pdftron/webviewer';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'pdf-viewer';
+export class AppComponent implements AfterViewInit {
+
+  @ViewChild('viewer') viewerRef!: ElementRef;
+
+  document = 'https://pdftron.s3.amazonaws.com/downloads/pl/webviewer-demo.pdf'
+
+  ngAfterViewInit(): void {
+      WebViewer({
+        path: '../assets/lib',
+        initialDoc: this.document
+      }, this.viewerRef.nativeElement).then(instance => {
+        instance.UI.setTheme('dark')
+        // instance.UI.loadDocument(this.document, {
+        //   customHeaders: {
+        //     'Access-Control-Allow-Origin': '*',
+        //     'Access-Control-Allow-Method': 'GET',
+        //     'Access-Control-Allow-Headers': 'authorization'
+        //   }
+        // })
+      })
+  }
+
+  // onChangeDocument() {
+  //   this.document = 'http://www.africau.edu/images/default/sample.pdf'
+  // }
+
 }
