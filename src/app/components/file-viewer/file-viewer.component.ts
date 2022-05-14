fa4dc30878ae51eea90020928aa23436431cd762
Ross carmack <rosscarmack@Rosss-MBP.lan>
2022-05-14 17:37:20 -0500

    app working

--- src/app/components/file-viewer/file-viewer.component.ts
+++ src/app/components/file-viewer/file-viewer.component.ts
@@ -11,48 +11,39 @@ import { FileUploadService } from 'src/app/services/file-upload.service';
 export class FileViewerComponent implements OnInit, OnDestroy, AfterViewInit {
 
   @ViewChild('viewer') viewerRef!: ElementRef;
-  @ViewChild('button1') button1!: HTMLButtonElement;
-  @ViewChild('button2') button2!: HTMLButtonElement;
   private fileChangeSub! : Subscription;
-
-  document = 'https://firebasestorage.googleapis.com/v0/b/pdfviewer-63816.appspot.com/o/uploads%2Fwebviewer-demo-annotated.pdf?alt=media&token=6e9e525f-2906-494c-9e39-a4eebe70caca'
-  document2 = 'https://firebasestorage.googleapis.com/v0/b/pdfviewer-63816.appspot.com/o/uploads%2FsamplePDF.pdf?alt=media&token=913283ff-43fe-41cb-93b2-9517c8ae3e3e'
+  webViewerInstance: any
+  
   constructor(private uploadService: FileUploadService) {}
 
 
   ngOnInit(): void {
       this.fileChangeSub = this.uploadService.fileChange.subscribe(value => {
-        console.log(value)
-        this.document = value
+        this.webViewerInstance.UI.loadDocument(value)
       })
     
   }
 
-  accessButtonEvent(event: any) {
-    console.log(event)
-  }
-
+  
   ngAfterViewInit(): void {
-    WebViewer({
-      path: '../../../assets/lib',
-      
-    }, this.viewerRef.nativeElement).then(instance => {
-      this.button1.addEventListener('click', () => {
-        instance.UI.loadDocument(this.document)
-      })
-      this.button2.addEventListener('click', () => {
-        instance.UI.loadDocument(this.document2)
-      })
-      // instance.UI.loadDocument(this.document)
-        
-      instance.UI.setTheme('dark')
+      WebViewer({
+    path: '../../../assets/lib',
+    useDownloader:false
+    
+  }, this.viewerRef.nativeElement).then(instance => {
 
-    })
+    this.webViewerInstance = instance
+      
+    instance.UI.setTheme('dark')
+
+  })
   }
 
   ngOnDestroy(): void {
   this.fileChangeSub.unsubscribe();
   }
+
+
 
 
 }
